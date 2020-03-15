5a3adfbfe79720fadf21677b5b77c8fcadb1b1f5
Marcin Swornowski <swor71@gmail.com>
2020-03-15 12:28:07 +0100

    Add styled components and initial consts

---
+++ src/consts/consts.ts
@@ -0,0 +1,12 @@
+export const rowAmount = 6;
+export const columnAmount = 7;
+export const winCondition = 4;
+
+export const emptyGrid = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+]
