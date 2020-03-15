530a1503d77be15bc8ed138587cd3330961823a2
Marcin Swornowski <swor71@gmail.com>
2020-03-15 23:27:08 +0100

    Add getNextPlayer function, tokenMargin const

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -4,6 +4,8 @@ export const rowAmount = 6;
 export const columnAmount = 7;
 export const winCondition = 4;
 
+export const tokenMargin = 5;
+
 export const emptyGrid: GridType = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
-export function getPlayerColor(playerNumber: number) {
-  switch (playerNumber) {
+export function getPlayerColor(currentPlayer: number) {
+  switch (currentPlayer) {
     case 1:
       return 'maroon';
     case 2:
@@ -8,4 +8,8 @@ export function getPlayerColor(playerNumber: number) {
     default:
       return 'ivory';
   }
-}
+};
+
+export function getNextPlayer(currentPlayer: number) {
+  return currentPlayer === 1 ? 2 : 1;
+};
