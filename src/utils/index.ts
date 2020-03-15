4d13f6be46bd9374e1a30a62fb10681c7866832e
Marcin Swornowski <swor71@gmail.com>
2020-03-15 14:01:36 +0100

    Add checkVertical logic

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,11 +10,12 @@ export function checkForWin(grid: GridType): boolean {
         if(checkHorizontal(grid, x, y)) {
           return true;
         }
+
+        if(checkVertical(grid, x, y)) {
+          return true;
+        }
       }
 
-      // if(checkVertical(grid, x, y)) {
-      //   return true;
-      // }
 
       // if(checkDiagonal(grid, x, y)) {
       //   return true;
@@ -28,31 +29,49 @@ export function checkForWin(grid: GridType): boolean {
 }
 
 export function checkHorizontal(grid: GridType, x: number, y: number): boolean {
-    const nextY = y + 1;
-    let found = 1;
 
-    for(let i = nextY; i < columnAmount; i++) {
-      if (grid[x][i] === 1) {
-        found++
-      }
+  const nextY = y + 1;
+  let found = 1;
 
-      if(found === winCondition) {
-        return true;
-      }
-
-      if (grid[x][i] === 0) {
-        found = 0;
-        break;
-      }
+  for(let i = nextY; i < columnAmount; i++) {
+    if (grid[x][i] === 1) {
+      found++
     }
 
-    return false;
+    if(found === winCondition) {
+      return true;
+    }
+
+    if (grid[x][i] === 0) {
+      found = 0;
+      break;
+    }
   }
 
+  return false;
+}
 
-// export function checkVertical(grid: GridType, x: number, y: number): boolean {
 
+export function checkVertical(grid: GridType, x: number, y: number): boolean {
 
+  const nextX = x - 1;
+  let found = 1;
 
-//   return false;
-// }
+  for(let i = nextX; i >= 0; i--) {
+    if (grid[i][y] === 1) {
+      found++
+    }
+    console.log(`grid[${i}][${y}] = ${grid[i][y]} found = ${found}`);
+
+    if(found === winCondition) {
+      return true;
+    }
+
+    if (grid[i][y] === 0) {
+      found = 0;
+      break;
+    }
+  }
+
+  return false;
+}
